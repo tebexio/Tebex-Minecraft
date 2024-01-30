96df4aacc67b7eef6c22be1408f83b219fceb8cb
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-01-30 12:32:18 -0600

    fix: allow null values when converting mojang uuids #17, #10

--- sdk/src/main/java/io/tebex/sdk/util/UUIDUtil.java
+++ sdk/src/main/java/io/tebex/sdk/util/UUIDUtil.java
@@ -8,9 +8,13 @@ public class UUIDUtil {
      * Mojang-style UUIDs.
      *
      * @param id the Mojang UUID to use
-     * @return the Java UUID
+     * @return the Java UUID or null if id provided is null
      */
     public static UUID mojangIdToJavaId(String id) {
+        if (id == null) {
+            return null;
+        }
+
         return UUID.fromString(id.replaceFirst(
                 "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
                 "$1-$2-$3-$4-$5"
