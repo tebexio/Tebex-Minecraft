8efec0604a46fd617665abd308d3caaf8b581913
heychazza <ch@rli.es>
2024-02-14 14:24:27 +0000

    WIP

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -5,7 +5,6 @@ version = rootProject.version
 
 dependencies {
     implementation(project(":sdk"))
-    implementation("it.unimi.dsi:fastutil:8.5.6")
     implementation("com.github.cryptomorin:XSeries:9.3.1") { isTransitive = false }
     implementation("dev.triumphteam:triumph-gui:3.1.2")
 
@@ -17,13 +16,8 @@ dependencies {
 tasks.named("shadowJar", ShadowJar::class.java) {
     configurations = listOf(project.configurations.runtimeClasspath.get())
 
-    relocate("it.unimi", "io.tebex.plugin.libs.fastutil")
-    relocate("okhttp3", "io.tebex.plugin.libs.okhttp3")
-    relocate("okio", "io.tebex.plugin.libs.okio")
     relocate("dev.dejvokep.boostedyaml", "io.tebex.plugin.libs.boostedyaml")
     relocate("org.jetbrains.annotations", "io.tebex.plugin.libs.jetbrains")
-    relocate("kotlin", "io.tebex.plugin.libs.kotlin")
-    relocate("com.github.benmanes.caffeine", "io.tebex.plugin.libs.caffeine")
     relocate("com.google.gson", "io.tebex.plugin.libs.gson")
     minimize()
 }
