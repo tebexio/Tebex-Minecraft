05035d93c2b344af16296b5c2c09bef2c0575c9e
Dusty A <WildBamaBoy@users.noreply.github.com>
2024-09-27 10:07:17 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ rm -f build/libs/*
 # Move all successful builds to build output folder
 mv bukkit/build/libs/tebex*.jar build/libs/
 mv bungeecord/build/libs/tebex*.jar build/libs/
-mv fabric/build/libs/tebex*.jar build/libs/
+mv fabric-1.20.4/build/libs/tebex*.jar build/libs/
 mv velocity/build/libs/tebex*.jar build/libs/
 mv sdk/build/libs/tebex*.jar build/libs/
