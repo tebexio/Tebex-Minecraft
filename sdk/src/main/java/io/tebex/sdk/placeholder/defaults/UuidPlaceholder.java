dec847927eddadb87f1c0f0de46738a018d6aa8b
MachoPiggies <danieljdean86@gmail.com>
2026-08-02 20:09:03 +0100

    Fix issue where {id} and {uuid} variables incorrectly gave the players name for Geyser players without attempting to resolve the XUID

--- sdk/src/main/java/io/tebex/sdk/placeholder/defaults/UuidPlaceholder.java
+++ sdk/src/main/java/io/tebex/sdk/placeholder/defaults/UuidPlaceholder.java
@@ -17,7 +17,19 @@ public class UuidPlaceholder implements Placeholder {
     @Override
     public String handle(QueuedPlayer player, String command) {
         if (player.getUuid() == null || player.getUuid().equals("null") || player.getUuid().equals(EMPTY_UUID.toString())) {
-            return placeholderManager.getUniqueIdRegex().matcher(command).replaceAll(player.getName());
+
+            // Bedrock players don't have UUIDs in the traditional sense.
+            // The UUIDs returned by Geyser are hexed versions of the XUID.
+            //
+            // For example: the XUID 2533274913322943 becomes 900000753B7BF
+            //  when converted from DEC to HEX, then Floodgate puts '00000000-0000-0000-000'
+            //  in front of it, so the UUID becomes '00000000-0000-0000-0009-00000753B7BF'.
+            //
+            // This should return the XUID as this is what the Tebex system currently intends
+            //  to use, but if they want to swap or someone wants a Floodgate UUID, this is how
+            //  they work.
+
+            return placeholderManager.getUniqueIdRegex().matcher(command).replaceAll(player.getXuid() != null ? player.getXuid() : player.getName());
         }
         return placeholderManager.getUniqueIdRegex().matcher(command).replaceAll(UUIDUtil.mojangIdToJavaId(player.getUuid()).toString());
     }
